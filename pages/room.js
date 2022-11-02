3d0aa42116fe07042a63e4dfe9c0e460037eb0b0
ismailkefokeris <ismailkefokeris@gmail.com>
2022-11-02 19:06:38 +0000

    Slight tweak

--- pages/room.js
+++ pages/room.js
@@ -5,6 +5,7 @@ import Card from "../components/card";
 
 
 export default function Room() {
+    const description = "dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius duis at consectetur lorem donec massa sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl";
     return (
         <div className="max-w-[80%] mx-auto mt-11">
 
@@ -18,7 +19,7 @@ export default function Room() {
                 <p className="cursor-pointer font-semibold mt-1 ml-1">RM505</p>
             </div>
 
-            <Card roomname="name" capacity={20} type="Computers" description="lorem40" image="room.png" />
+            <Card roomname="RM505" capacity={20} type="Computers" description={description} image="room.png" />
 
         </div>
     );
